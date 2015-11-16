cd3248f1d48d5c3999af3f3caa7458ce04b8942b
David Mekerishvili <paperdavid@gmail.com>
2015-11-16 20:58:44 +0300

    News

--- build/js/main.js
+++ build/js/main.js
@@ -13257,35 +13257,6 @@ $(document).ready(function() {
     	window.TiltFx = TiltFx;
     
     })(window);
-    ymaps.ready(init);
-    
-    function init () {
-        var myMap = new ymaps.Map('map', {
-                center: [57.242246, 77.848838],
-                zoom: 4
-            }, {
-                searchControlProvider: 'yandex#search'
-            }),
-            objectManager = new ymaps.ObjectManager({
-                // Чтобы метки начали кластеризоваться, выставляем опцию.
-                clusterize: true,
-                // ObjectManager принимает те же опции, что и кластеризатор.
-                gridSize: 32
-            });
-    
-        // Чтобы задать опции одиночным объектам и кластерам,
-        // обратимся к дочерним коллекциям ObjectManager.
-        objectManager.objects.options.set('preset', 'islands#greenDotIcon');
-        objectManager.clusters.options.set('preset', 'islands#greenClusterIcons');
-        myMap.geoObjects.add(objectManager);
-    
-        $.ajax({
-            url: "../../../data.json"
-        }).done(function(data) {
-            objectManager.add(data);
-        });
-    
-    }
 
 
 
